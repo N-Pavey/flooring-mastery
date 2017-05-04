b95c899fcd03eb58717765bbd20c86e00ae479c6
Nathan Pavey <nathanfpavey@gmail.com>
2017-05-03 21:13:45 -0400

    Interested Calculator has been refactored.

---
+++ S02/S02-Exercises/RefactoredInterestCalculator.java/src/main/java/com/mycompany/refactoredinterestcalculator/java/App.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.refactoredinterestcalculator.java;
+
+/**
+ *
+ * @author apprentice
+ */
+public class App {
+    
+    public static void main(String[] args) {
+        
+        InterestCalculator myInterestCalculator = new InterestCalculator();
+        myInterestCalculator.calculateInterest();
+        
+    }
+    
+}
