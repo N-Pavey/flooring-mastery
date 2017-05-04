a41d37f8aeda4e50fa76e7cac41034a2876aca50
Nathan Pavey <nathanfpavey@gmail.com>
2017-05-03 21:07:10 -0400

    Factorizer has been refactored.

---
+++ S02/S02-Exercises/RefactoredFactorizer.java/src/main/java/com/mycompany/refactoredfactorizer/java/RefactoredFactorizer.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.refactoredfactorizer.java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author apprentice
+ */
+
+
+public class RefactoredFactorizer {
+    
+    public void factorNum() {
+        
+        Scanner userInput = new Scanner(System.in);
+        
+        int primeCounter = 0;
+        int perfectNum = 0;
+        
+        System.out.print("What number would you like to factor? ");
+        int factorNum = userInput.nextInt();
+        
+        System.out.println("The factors of " + factorNum + " are:");
+        System.out.println("");
+        
+        for (int i = 1; i < factorNum; i++) {
+            
+            if (factorNum % i == 0) {
+                System.out.println(i);
+                primeCounter++;
+                perfectNum += i;
+            }
+            
+        }
+        
+        System.out.println("");
+        
+        if (perfectNum == factorNum) {
+            System.out.println(factorNum + " is a perfect number.");
+        } else {
+            System.out.println(factorNum + " is not a perfect number.");
+        }
+        
+        if (primeCounter == 1) {
+            System.out.println(factorNum + " is a prime number.");
+        } else {
+            System.out.println(factorNum + " is not a prime number.");
+        }
+        
+    }
+    
+}
